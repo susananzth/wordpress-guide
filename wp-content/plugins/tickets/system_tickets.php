480aa198d2c1126edd2b0737743f74ecd247aac4
Susana Piñero <susananzth@gmail.com>
2020-08-02 17:39:53 -0500

    Agregando información del plugin

---
+++ wp-content/plugins/tickets/system_tickets.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Tickets de atención al usuario
+Plugin URI:
+Description: Plugin para creación y gestion de Tickets de atención al usuarios.
+Version: 1.0
+Author: Susana Piñero Rodríguez
+Author URI: https://susananzth.com/
+License: GPL2+
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: ticket
+Domain Path: /languages
+*/
+
+?>
