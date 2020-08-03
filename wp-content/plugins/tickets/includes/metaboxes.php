c705e5541a664be999d4d4e39e9fa85d39e432cc
Susana Piñero <susananzth@gmail.com>
2020-08-02 19:43:03 -0500

    Agregando mas campos en metaboxes

--- wp-content/plugins/tickets/includes/metaboxes.php
+++ wp-content/plugins/tickets/includes/metaboxes.php
@@ -4,7 +4,6 @@ add_action( 'admin_init', 'ticket_add_metaboxes', 1 );
 
 function ticket_add_metaboxes()
 {
-
     add_meta_box(
          'ticket_meta_box_name'
         ,'Respuestas'
@@ -21,8 +20,52 @@ function render_meta_box_content()
     <table class="form-table">
       <tr>
         <th class="row-title">
-          <h2>A;ade las respuestas aqui</h2>
+          <h2>Ticket de soporte</h2>
         </th>
+      </tr>
+      <tr>
+        <th class="row-title">
+          <label for="name">Nombre completo:</label>
+        </th>
+        <td>
+          <input type="text" id="name" name="name" placeholder="Su nombre" class="regular'text">
+        </td>
+      </tr>
+      <tr>
+        <th class="row-title">
+          <label for="email">Correo electrónico:</label>
+        </th>
+        <td>
+          <input type="text" id="email" name="email" placeholder="Ej: correo@ejemplo.com" class="regular'text">
+        </td>
+      </tr>
+      <tr>
+        <th class="row-title">
+          <label for="type">Tipo de solicitud:</label>
+        </th>
+        <td>
+          <select id="type" name="type" class="postbox">
+            <option value="">Consulta</option>
+            <option value="">Solicitud</option>
+            <option value="">Reclamo</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <th class="row-title">
+          <label for="title">Título:</label>
+        </th>
+        <td>
+          <input type="text" id="title" name="title" class="regular'text">
+        </td>
+      </tr>
+      <tr>
+        <th class="row-title">
+          <label for="content">Contenido:</label>
+        </th>
+        <td>
+          <textarea id="content" name="content" rows="8" cols="80" placeholder="Describa detalladamente su solicitud..."></textarea>
+        </td>
       </tr>
     </table>
   <?php
