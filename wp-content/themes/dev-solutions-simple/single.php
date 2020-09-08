0f9b3c3710ac65a712aec8c7575a40b6c4b0e404
Susana Piñero <susananzth@gmail.com>
2020-09-08 18:47:19 -0500

    Agregando diseño en simple.php del theme simple

--- wp-content/themes/dev-solutions-simple/single.php
+++ wp-content/themes/dev-solutions-simple/single.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?><!-- Call to header partial -->
-			
+
 			<section id="main-content">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <!-- Si hay posts, mientras haya posts, muestra el post -->
-				
+
 				<article class="post">
 					<header class="post-title">
 						<h1><?php the_title(); ?></h1>
@@ -18,7 +18,26 @@
 				</article>	<!-- article -->
 				<?php endwhile; endif ?>
 			</section><!-- /#main-content -->
-			
+
 			<?php get_sidebar(); ?><!-- Call to sidebar partial -->
-			
+
 <?php get_footer(); ?><!-- Call to footer partial -->
+
+
+<section id="global-content-fullwidth" class="cf">
+		<section id="main-content" class="fullwidth">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<!-- Si hay posts, mientras haya posts, muestra el post -->
+				<article class="post">
+						<header class="post-title">
+								<h1><?php the_title(); ?></h1>
+								<!-- Muestra el título del post -->
+						</header>
+						<div class="post-content">
+								<?php the_content(); ?>
+								<!-- Muestra el artículo -->
+						</div>
+				</article>	<!-- article -->
+				<?php endwhile; endif ?>
+		</section><!-- /#main-content -->
+</section><!-- /#global-content -->
